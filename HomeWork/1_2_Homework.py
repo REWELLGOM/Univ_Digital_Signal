08d771ec519699e53eecc9731e6b6d59a33f5d7a
seong mok lee <deadmouse210@naver.com>
2024-03-17 17:56:02 +0900

    1_2_HW

--- HomeWork/1_2_Homework.py
+++ HomeWork/1_2_Homework.py
@@ -1,26 +1,41 @@
+"""
 mylist=[10,0,-12,"list","char","100",50]
 mylist[-1]
 print(mylist[6])
 print(mylist[:4])
 print(mylist[5:6])
 print(mylist[3:6])
+"""
 
+"""
 
+mylist=[10,0,-12,"list","char","100",50]
 print(mylist[-1])
 print(mylist[-7:-3])
 print(mylist[-2:])
 print(mylist[-4:-1])
+"""
 
+
+
+"""
 myList = [["aaa", "bbb", "ccc", "ddd"], [40,30,20,10]]
 print(myList[0][1])
 print(myList[1][3])
 print(myList[0][1:3])
 print(myList[1][1:4])
 
+"""
+"""
+
+myList = [["aaa", "bbb", "ccc", "ddd"], [40,30,20,10]]
 print(myList[-2][-3])
 print(myList[-1][-1])
 print(myList[-2][-3:-1])
 print(myList[-1][-3:])
+"""
+
+"""
 
 MyList=[[200,100,["AAA","BBB","CCC","DDD"]],"C++"]
 print(MyList[0][2][1])
@@ -28,3 +43,11 @@ print(MyList[0][2][3])
 
 print(MyList[-2][-1][-3])
 print(MyList[-2][-1][-1])
+"""
+
+myList = [100,200,300]
+myList[1] = 2
+print(myList)
+
+
+
