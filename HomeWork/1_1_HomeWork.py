5fad1232a9305b74b82564f1e3b46a2da6a15158
seong mok lee <deadmouse210@naver.com>
2024-03-17 17:55:30 +0900

    1_1_HW

---
+++ HomeWork/1_1_HomeWork.py
@@ -0,0 +1,106 @@
+"""
+first_member_age = 20
+second_member_age = 21
+
+TotAge = first_member_age + second_member_age
+print("The sum of ages is: ", TotAge)
+"""
+
+"""
+number1 = 17
+number2 = 3
+
+div = divmod(number1,number2)
+q = div[0]
+r = div[1]
+
+print("몫 값: " ,q, "\n나머지값: ",r)
+"""
+
+"""
+x = 2
+y = 3
+x **= y
+print(x)
+"""
+
+"""
+
+ch_1 = input("휴대전화 국번호를 입력하시오: ")
+ch_2 = input("\n휴대전화 앞번호를 입력하시오 ")
+ch_3 = input("\n휴대전화 뒷번호를 입력하시오 ")
+
+print(ch_1+"-"+ch_2+"-"+ch_3)
+
+"""
+
+"""
+Math = input("수학 성적을 입력하시오: ")
+Sci = input("과학 성적을 입력하시오: ")
+Eng = input("영어 성적을 입력하시오 ")
+
+sum = int(Math) + int(Sci) + int(Eng)
+print("합계 점수 = ", sum)
+
+avg= (int(sum))/3
+print("평균 점수 = ", avg)
+"""
+
+"""
+val1 = list(range(0,10,3))
+print(val1)
+
+"""
+"""
+
+myStr = "We all love Python programming!"
+print(myStr[30])
+print(myStr[:11])
+print(myStr[7:])
+print(myStr[12:18])
+"""
+
+"""
+
+myStr = "We all love Python programming!"
+print(myStr[-1:])
+print(myStr[:-20])
+print(myStr[-24:])
+print(myStr[-19:-12])
+"""
+"""
+
+char_1 = "Python"
+char_2 = "Java"
+
+charSum = char_1 + char_2
+print(charSum)
+
+unitChar = "#"
+print(unitChar*3)
+print("Python")
+print(unitChar*3)
+"""
+
+"""
+
+myStr = "Python Java C++ Prolog"
+print(myStr.replace(" ",","))
+print(myStr.replace(" ","/"))
+print(myStr.replace(" ","-"))
+"""
+
+print("I learned {0} in {1} for {2} days".format("Python","Korea",3))
+print("{0:8.4f}".format(1.414325432))
+
+
+
+
+
+
+
+
+
+
+
+
