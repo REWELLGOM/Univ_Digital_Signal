46ff404cd87d9336f2550ee7ec346983a9e9d5a7
MOK <deadmouse210@naver.com>
2024-04-18 11:17:05 +0900

    OpenCv

--- HomeWork/OpenCv3.py
+++ HomeWork/OpenCv3.py
@@ -2,6 +2,14 @@ import cv2
 video_file = "img/img/big_buck.avi"
 
 cap = cv2.VideoCapture(video_file)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,320)
+nframe = cap.set(cv2.CAP_PROP_FRAME_COUNT)
+fps = cap.get(cv2.CAP_PROP_FPS)
+delay = int (500/fps)
+cap.set(cv2.CAP_PROP_POS_MSEC,int(nframe*fps)//2)
+print("FPS: %f, Delay: %dms" %(fps, delay))
+
 while cap.isOpened():
     ret, img = cap.read()
     if ret:
